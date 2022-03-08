d40443df6c2217a95d6f1b3ce95bf0bc73d009b3
Alice <alice890308@gmail.com>
2022-03-08 17:10:30 +0800

    gRPC TODO

--- modules/comment/service/service.go
+++ modules/comment/service/service.go
@@ -28,7 +28,7 @@ func (s *service) Healthz(ctx context.Context, req *pb.HealthzRequest) (*pb.Heal
 }
 
 /*
-TODO: 1. Call dao API to list comment and check if any error happen. If so, return an nil response.
+gRPC TODO: 1. Call dao API to list comment and check if any error happen. If so, return an nil response.
 	  2. Create an array to store the return value. Remember to transform the data schema from dao into protobuf.
 	  	 (Refer to the ToProto method defined in dao/comment.go. This method is used to transform data schema from dao into protobuf.)
 	  3. Pack the array into correct format.
@@ -37,7 +37,7 @@ func (s *service) ListComment(ctx context.Context, req *pb.ListCommentRequest) (
 }
 
 /*
-TODO: 1. Send gRPC to video server to check if the video id in request is valid. Think about which gRPC provided by video server to use?
+gRPC TODO: 1. Send gRPC to video server to check if the video id in request is valid. Think about which gRPC provided by video server to use?
 		 If any error happened, return nil response and the error.
 	  2. Create a comment with information in request.
 	  3. Call dao API to create a new comment and do error handling.
@@ -47,7 +47,7 @@ func (s *service) CreateComment(ctx context.Context, req *pb.CreateCommentReques
 }
 
 /*
-TODO: 1. Update a comment with information in request.
+gRPC TODO: 1. Update a comment with information in request.
 	  2. Call dao API to update a comment and do error handling. You need to handle comment not found error and other unknown error here.
 	  3. Return the result. Don't forget to transform the data schema from dao into proto.
 	  	 (Refer to the ToProto method defined in dao/comment.go. This method is used to transform data schema from dao into protobuf.)
@@ -60,7 +60,7 @@ func (s *service) UpdateComment(ctx context.Context, req *pb.UpdateCommentReques
 }
 
 /*
-TODO: 1. Call dao API to delete a comment and do error handling. You need to handle comment not found error and other unknown error here.
+gRPC TODO: 1. Call dao API to delete a comment and do error handling. You need to handle comment not found error and other unknown error here.
 	  2. Return the response.
 */
 func (s *service) DeleteComment(ctx context.Context, req *pb.DeleteCommentRequest) (*pb.DeleteCommentResponse, error) {
@@ -71,7 +71,7 @@ func (s *service) DeleteComment(ctx context.Context, req *pb.DeleteCommentReques
 }
 
 /*
-TODO: 1. Call dao API to delete comments by video id and do error handling. You need to do error handling here.
+gRPC TODO: 1. Call dao API to delete comments by video id and do error handling. You need to do error handling here.
 	  2. Return the response.
 */
 func (s *service) DeleteCommentByVideoID(ctx context.Context, req *pb.DeleteCommentByVideoIDRequest) (*pb.DeleteCommentByVideoIDResponse, error) {
